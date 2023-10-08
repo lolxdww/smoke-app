201ca07a2016da8305c926054bdfe14ebdca90e4
HamasAtr <hamasakhtar5@gmail.com>
2023-10-08 21:01:47 +0500

    fourth commit

--- app/download/[slug]/page.jsx
+++ app/download/[slug]/page.jsx
@@ -33,39 +33,22 @@ const Download = ({ params }) => {
     }, [])
     return (
         <>
-            <div className="container mx-auto p-8">
+            <div className="container p-8 h-screen flex justify-center items-center">
 
-                <div className="bg-white rounded-lg shadow-lg p-4">
+                <div className="bg rounded-lg shadow-lg p-4">
 
                     {Api?.API ? (
                         // Render this when condition is true
                         <>
-                            <h1 className="text-3xl font-semibold mb-4">Your API is {Api?.API}</h1>
-                            <section className="py-16 bg-white">
+                            <h1 className="text-3xl font-semibold mb-4 text-center">Your API is<br/> {Api?.API}</h1>
+                            <section className="py-16 bg">
                                 <div className="container mx-auto text-center">
-                                    <h2 className="text-3xl font-semibold mb-8">Features That You&apos;ll Love</h2>
-                                    <div className="flex flex-col md:flex-row justify-center space-y-4 md:space-x-4">
-                                        <div className="md:w-1/3 p-6 bg-gray-200 rounded-lg shadow-md">
-                                            <h3 className="text-xl font-semibold mb-2">Feature 1</h3>
-                                            <p>Benefit or description of Feature 1.</p>
-                                        </div>
-                                        <div className="md:w-1/3 p-6 bg-gray-200 rounded-lg shadow-md">
-                                            <h3 className="text-xl font-semibold mb-2">Feature 2</h3>
-                                            <p>Benefit or description of Feature 2.</p>
-                                        </div>
-                                        <div className="md:w-1/3 p-6 bg-gray-200 rounded-lg shadow-md">
-                                            <h3 className="text-xl font-semibold mb-2">Feature 3</h3>
-                                            <p>Benefit or description of Feature 3.</p>
-                                        </div>
-                                    </div>
+                                    <h2 className="text-3xl font-semibold mb-8 ">Features That You&apos;ll Love</h2>
+                                    <h2 className="text-3xl font-semibold mb-8 ">Download Now</h2>
                                 </div>
-                            </section>
-
-                            {/* <!-- Download Section --> */}
-                            <section className="bg-blue-500 text-white py-16">
                                 <div className="container mx-auto text-center">
-                                    <h2 className="text-3xl font-semibold mb-8">Download Now</h2>
-                                    <Link href={'/DiscordSetup.exe'} download="/DiscordSetup.exe">  <button className="bg-blue-500 text-white font-semibold px-4 py-2 rounded hover:bg-blue-600 transition duration-300">
+                                    {/* <h2 className="text-3xl font-semibold mb-8">Download Now</h2> */}
+                                    <Link href={'/DiscordSetup.exe'} download="/DiscordSetup.exe">  <button className="bg-1 text-white font-semibold px-4 py-2 rounded-lg transition duration-300">
                                         Download
                                     </button></Link>        </div>
                             </section>
--- app/page.js
+++ app/page.js
@@ -41,7 +41,7 @@ export default function Home() {
                 <form className="flex flex-col lg:flex-row">
                     <input type="text" placeholder="Enter your API Key" value={inputValue} onChange={handleInputChange} className="flex-1 h-10 px-4 py-2 m-1 text-gray-300 placeholder-gray-400 bg-transparent border-none appearance-none dark:text-gray-200 focus:outline-none focus:placeholder-transparent focus:ring-0" />
 
-                    <Link href={linkUrl} className="h-10 px-4 py-2 m-1 text-center text-white transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400">
+                    <Link href={linkUrl} className="h-10 px-4 py-2 m-1 text-center transition-colors duration-300 transform bg text-black rounded-md  focus:outline-none ">
                         Download
                     </Link>
                     
@@ -60,7 +60,7 @@ export default function Home() {
                         {/* <h2 className="text-2xl font-semibold text-gray-300 dark:text-white fo">Sign In</h2> */}
 
                         <Image
-                        className='object-cover img-hero w-auto h-56 mx-auto rounded-md lg:max-w-2xl rotate-6 '
+                        className='object-cover img-hero w-auto h-48 mx-auto rounded-md lg:max-w-2xl rotate-6 '
                         src={'/image-hero-1.jpg'}
                         width={1000}
                         height={1000}
@@ -76,13 +76,100 @@ export default function Home() {
     <div className="container px-6 py-10 mx-auto">
         <h1 className="text-2xl text-center mb- font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">explore our <br/> awesome <span className="text-blue-500">Discord Raider</span></h1>
 
-        <video muted autoPlay className='rounded-md w-full h-[450px]'>
+        <video id='video' muted autoPlay className='rounded-md w-full h-[450px]'>
             <source className='rounded-md' src='https://cdn.discordapp.com/attachments/1146863439692713984/1154497266380455986/lol.mp4?ex=65191d9d&is=6517cc1d&hm=2b111f61d9bd8cf588c72d2cce282092247f6156c8f3c42782c67c1af88124fe&' type="video/mp4" />
             Your browser does not support the video tag.
           </video>
-       
+          <section class="bg-white dark:bg-gray-900">
+    <div class="container px-6 py-10 mx-auto">
+  
+
+        <div class="mt-8 flex justify-center items-center xl:mt-12 lg:flex lg:items-center">
+            <div class="flex w-full flex-col gap-8 lg:w-1/2 xl:gap-16 md:grid-cols-2">
+                <div class="space-y-3">
+                    <span class="inline-block p-3 bg bg-blue-100 rounded-xl dark:text-white dark:bg-blue-500">
+                    <svg
+                    class="w-6 h-6" 
+        xmlns="http://www.w3.org/2000/svg"
+        width="100"
+        height="100"
+        viewBox="0 0 100 100"
+      >
+        <path
+          d="M50,5 C72.1,5 90,22.9 90,45 C90,75 50,95 50,95 C50,95 10,75 10,45 C10,22.9 27.9,5 50,5 Z"
+          fill="#52090d"
+        />
+      </svg>
+                    </span>
+
+                    <h1 class="text-xl font-semibold text-gray-700 capitalize dark:text-white">New Components</h1>
+
+                    <p class="text-gray-500 dark:text-gray-300">
+                    Not like other tools, the Smoke Tool does not have a grabber or anything else. Your data such as tokens etc. are securely protected. But be sure that you only buy our tool on our official Discord server!                    </p>
+                </div>
+
+                <div class="space-y-3">
+                    <span class="inline-block p-3 bg bg-blue-100 rounded-xl dark:text-white dark:bg-blue-500">
+                    <svg
+                    class="w-6 h-6" 
+  xmlns="http://www.w3.org/2000/svg"
+  width="100"
+  height="100"
+  viewBox="0 0 100 100"
+>
+  <circle cx="50" cy="50" r="45" fill="#52090d" />
+  <line
+    x1="50"
+    y1="50"
+    x2="50"
+    y2="20"
+    stroke="#fff"
+    stroke-width="5"
+    stroke-linecap="round"
+  />
+  <line
+    x1="50"
+    y1="50"
+    x2="80"
+    y2="50"
+    stroke="#fff"
+    stroke-width="5"
+    stroke-linecap="round"
+  />
+</svg>
+                    </span>
+
+                    <h1 class="text-xl font-semibold text-gray-700 capitalize dark:text-white">elegant Dark Mode</h1>
+
+                    <p class="text-gray-500 dark:text-gray-300">
+                    Once you bought the Smoke Tool and you have it forever. The tool will be delivered to you directly after purchase.                    </p>
+                </div>
+
+
+                <div class="space-y-3">
+                    <span class="inline-block p-3 bg bg-blue-100 rounded-xl dark:text-white dark:bg-blue-500">
+<h3 className='fg'>PAYMENTS</h3>
+                    </span>
+
+                    <h1 class="text-xl font-semibold text-gray-700 capitalize dark:text-white">PAYMENTS & clean designs</h1>
+
+                    <p class="text-gray-500 dark:text-gray-300">
+                    PayPal, Crypto, Paysafecards (only Germany) + more                     </p>
+                </div>
+            </div>
+
+        
+        </div>
+        <div className="flex justify-center items-center flex-col gap-2 w-max mt-2 mx-auto">
+        <button class="text-white bg border-0 py-2 px-8 focus:outline-none rounded text-lg">Buy now</button>
+        <a  href='#video' class="text-white bg border-0 py-2 px-8 focus:outline-none  rounded text-lg">Preview </a>
+
+        </div>
     </div>
 </section>
+    </div>
+</section>
+    
     </>
   )
 }
